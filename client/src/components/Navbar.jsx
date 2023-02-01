ef1b5b93aaddb98522f567e1c2a4d951b4ca6c2b
kristenking <kristenking0110@gmail.com>
2023-01-31 23:57:00 -0500

    Added dark theme

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,23 @@
-// import {
-//   Box,
-//   Flex,
-//   HStack,
-//   Link,
-//   IconButton,
-//   Icon,
-//   Text,
-//   useDisclosure,
-//   Button,
-//   Stack,
-//   useColorModeValue,
-//   useColorMode,
-// } from "@chakra-ui/react";
-// import { Link as ReactLink } from "react-router-dom";
-// import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
-// import { GiRobotLeg } from "react-icons/gi";
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Icon,
+  Text,
+  useDisclosure,
+  Button,
+  Stack,
+  useColorModeValue,
+  useColorMode,
+  Image,
+  Input
+} from "@chakra-ui/react";
+import { Link as ReactLink } from "react-router-dom";
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import React from "react";
+import { GiRobotLeg } from "react-icons/gi";
 
 // const Navbar = () => {
 //   const { isOpen, onClose, onOpen } = useDisclosure()
@@ -43,12 +46,12 @@
 // };
 
 // export default Navbar;
-import React from "react";
-import { Box, Image, Input, Link, useColorMode, Icon, Text} from "@chakra-ui/react";
-import { GiRobotLeg } from "react-icons/gi";
+
 
 const Navbar = () => {
-  const { colorMode } = useColorMode();
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
   const bgColor = { light: "teal.900", dark: "teal.200" };
   const color = { light: "white", dark: "gray.800" };
 
@@ -61,12 +64,11 @@ const Navbar = () => {
       bg={bgColor[colorMode]}
       color={color[colorMode]}
     >
-  
       <Box display="flex" flex={1} alignItems="center">
-      <Icon as={GiRobotLeg} h={10} width={10} mr={2} color='green.400'/>
-      <Text fontWeight='extrabold'>Tech Wonders</Text>
+        <Icon as={GiRobotLeg} h={10} width={10} mr={2} color='green.400' />
+        <Text fontWeight='extrabold'>Tech Wonders</Text>
       </Box>
-     
+
       <Box display="flex" alignItems='center'>
         <Link to="/" mx={2}>
           Home
@@ -77,19 +79,25 @@ const Navbar = () => {
         <Link to="/about" mx={2}>
           About
         </Link>
-        <Box mr={2}>
-        <Input placeholder="Search products..." />
-      </Box>
+        <Box mr={3}>
+          <Input placeholder="Search products..." />
+        </Box>
         <Link to="/cart">
           <Box
             as="span"
             px={2}
             py={1}
-            bg="gray.200"
+            bg="#53B8BB"
             rounded="md"
             color={color[colorMode]}
           >
             Cart
+          </Box>
+        </Link>
+        <Link >
+          <Box px={3} display="flex" alignSelf="center">
+            <Icon as={colorMode === 'light' ? MoonIcon : SunIcon}
+              onClick={() => toggleColorMode()} />
           </Box>
         </Link>
       </Box>
