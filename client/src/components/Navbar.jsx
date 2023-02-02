c1390f56362138a50d99ec0ed47ad5ccc3456b64
kristenking <kristenking0110@gmail.com>
2023-02-01 23:05:32 -0500

    Commit before working on responsive navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,35 +19,6 @@ import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 import React from "react";
 import { GiRobotLeg } from "react-icons/gi";
 
-// const Navbar = () => {
-//   const { isOpen, onClose, onOpen } = useDisclosure()
-//   const { colorMode, toggleColorMode } = useColorMode()
-//   return (
-//     <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-//       <Flex h={16} alignItems="center" justifyContent="space-between">
-//         <IconButton
-//           size="md"
-//           icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-//           display={{ md: "none" }}
-//           onClick={isOpen ? onClose : onOpen}
-//         />
-//         <HStack>
-//           <Link as={ReactLink} to="/">
-//             <Flex alignItems='center'>
-// <Icon as={GiRobotLeg} h={6} width={6} color='green.400'/>
-// <Text fontWeight='extrabold'>Tech Wonders</Text>
-
-//             </Flex>
-//           </Link>
-//         </HStack>
-//       </Flex>
-//     </Box>
-//   );
-// };
-
-// export default Navbar;
-
-
 const Navbar = () => {
 
   const { isOpen, onClose, onOpen } = useDisclosure();
