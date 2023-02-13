26ee26417c9feeb5458a1e4e802fcff95b038b4c
kristenking <kristenking0110@gmail.com>
2023-02-12 23:34:36 -0500

    created ProductsScreen

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {ChakraProvider} from '@chakra-ui/react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
+import ProductsScreen from './screens/ProductsScreen';
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
     <Router>
       <Navbar />
       <main>
-        {/* <Routes></Routes> */}
+        <Routes>
+        <Route path='/products' element={<ProductsScreen />} ></Route>
+        </Routes>
       </main>
     </Router>
   </ChakraProvider>
