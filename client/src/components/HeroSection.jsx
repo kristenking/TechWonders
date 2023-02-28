3a073c8b0e38813b1d79f761bca71860dcb87daf
kristenking <kristenking0110@gmail.com>
2023-02-28 13:51:25 -0500

    aligned teaxt and image, added button,  styles

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Box, Text, Image } from '@chakra-ui/react';
+import { Box, Text, Heading, Image, Button } from '@chakra-ui/react';
 import robot1 from '../assets/robot1.png';
 
 const HeroSection = () => {
   return (
     <Box display="flex" alignItems="center" justifyContent="center" h="100vh">
-      <Box maxW="600px" mx="auto" px="4" display="flex" alignItems="center" order="2">
-        <Image src={robot1} w="90%" mr="200"/>
+      <Box mx="auto" px="4" display="flex" alignItems="center" order="2" mr="100px" w="300vh">
+        <Image src={robot1} w="100%" />
       </Box>
-      <Text order="1" w="30%" ml="4" fontSize="3xl" ml="200">
-        Hello, Human! Welcome to the world of modern robots. 
-        Here you can learn about the latest models, interact 
-        with our friendly AI, and maybe even take one home with you.
-      </Text>
+      <Box ml="200" mt="-100" color="#ededed" mr="100">
+        <Heading as="h1" fontSize="3xl" mb="10">Hello, Human!</Heading>
+        <Text fontSize="25px">
+          Welcome to the world of modern robots. Here you can learn about the latest models, interact 
+          with our friendly AI, and maybe even take one home with you.
+        </Text>
+        <Box display="flex" justifyContent="flex-end" >
+          <Button bgColor="#53B8BB" size="lg" mt={5} borderRadius={14}>Explore</Button>
+        </Box>
+      </Box>
     </Box>
   );
 };
